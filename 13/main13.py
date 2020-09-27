9b3e9dff73ba702cc367eb73bb2e0f2014e12d37
SammyakR <rokadesammyak@gmail.com>
2020-09-28 00:40:27 +0530

    13th level with xmlrpc.clent

--- 13/main13.py
+++ 13/main13.py
@@ -1,5 +1,19 @@
 import requests
+import xmlrpc.client
 
-req_php = requests.get("http://www.pythonchallenge.com/pc/phonebook.php")
 
-print(req_php.text)
+
+req_php = xmlrpc.client.ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
+
+print(req_php)
+print(req_php.system.listMethods())
+
+print(req_php.system.methodHelp('phone'))
+
+print(req_php.phone('evil'))
+print(req_php.phone('He'))
+print(req_php.phone('call'))
+print(req_php.phone('him'))
+
+print(req_php.phone('Bert'))
+
